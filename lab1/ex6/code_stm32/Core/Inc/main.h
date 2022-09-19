777669a485279e8c1d0ccefe95e5984eb1dc3abb
potato-stack <hatrungquyen89@gmail.com>
2022-09-20 02:30:46 +0700

    ex6

--- lab1/ex6/code_stm32/Core/Inc/main.h
+++ lab1/ex6/code_stm32/Core/Inc/main.h
@@ -58,46 +58,30 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_RED_1_Pin GPIO_PIN_1
-#define LED_RED_1_GPIO_Port GPIOA
-#define LED_YELLOW_1_Pin GPIO_PIN_2
-#define LED_YELLOW_1_GPIO_Port GPIOA
-#define LED_GREEN_1_Pin GPIO_PIN_3
-#define LED_GREEN_1_GPIO_Port GPIOA
-#define LED_RED_Pin GPIO_PIN_5
-#define LED_RED_GPIO_Port GPIOA
-#define LED_YELLOW_Pin GPIO_PIN_6
-#define LED_YELLOW_GPIO_Port GPIOA
-#define LED_GREEN_Pin GPIO_PIN_7
-#define LED_GREEN_GPIO_Port GPIOA
-#define a_Pin GPIO_PIN_0
-#define a_GPIO_Port GPIOB
-#define b_Pin GPIO_PIN_1
-#define b_GPIO_Port GPIOB
-#define c_Pin GPIO_PIN_2
-#define c_GPIO_Port GPIOB
-#define d1_Pin GPIO_PIN_10
-#define d1_GPIO_Port GPIOB
-#define e1_Pin GPIO_PIN_11
-#define e1_GPIO_Port GPIOB
-#define f1_Pin GPIO_PIN_12
-#define f1_GPIO_Port GPIOB
-#define g1_Pin GPIO_PIN_13
-#define g1_GPIO_Port GPIOB
-#define d_Pin GPIO_PIN_3
-#define d_GPIO_Port GPIOB
-#define e_Pin GPIO_PIN_4
-#define e_GPIO_Port GPIOB
-#define f_Pin GPIO_PIN_5
-#define f_GPIO_Port GPIOB
-#define g_Pin GPIO_PIN_6
-#define g_GPIO_Port GPIOB
-#define a1_Pin GPIO_PIN_7
-#define a1_GPIO_Port GPIOB
-#define b1_Pin GPIO_PIN_8
-#define b1_GPIO_Port GPIOB
-#define c1_Pin GPIO_PIN_9
-#define c1_GPIO_Port GPIOB
+#define pin_1_Pin GPIO_PIN_4
+#define pin_1_GPIO_Port GPIOA
+#define pin_2_Pin GPIO_PIN_5
+#define pin_2_GPIO_Port GPIOA
+#define pin_3_Pin GPIO_PIN_6
+#define pin_3_GPIO_Port GPIOA
+#define pin_4_Pin GPIO_PIN_7
+#define pin_4_GPIO_Port GPIOA
+#define pin_5_Pin GPIO_PIN_8
+#define pin_5_GPIO_Port GPIOA
+#define pin_6_Pin GPIO_PIN_9
+#define pin_6_GPIO_Port GPIOA
+#define pin_7_Pin GPIO_PIN_10
+#define pin_7_GPIO_Port GPIOA
+#define pin_8_Pin GPIO_PIN_11
+#define pin_8_GPIO_Port GPIOA
+#define pin_9_Pin GPIO_PIN_12
+#define pin_9_GPIO_Port GPIOA
+#define pin_10_Pin GPIO_PIN_13
+#define pin_10_GPIO_Port GPIOA
+#define pin_11_Pin GPIO_PIN_14
+#define pin_11_GPIO_Port GPIOA
+#define pin_12_Pin GPIO_PIN_15
+#define pin_12_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
